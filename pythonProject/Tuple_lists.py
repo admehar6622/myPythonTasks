783fac6077540f03deee5a7f6f982d3acea05f3c
admehar6622 <nomanatif6601@gmail.com>
2024-01-09 16:39:03 +0500

    tuple list

--- pythonProject/Tuple_lists.py
+++ pythonProject/Tuple_lists.py
@@ -1,1 +1,9 @@
-print("Tuple list")
+tup = (1,2,4,6,10,15,20)
+print (tup[:5])
+
+
+countries1 = ("Pakistan", "india", "Bangladesh")
+countries2 = ("srilank","maldives", "nepal")
+
+Southasia = countries2 + countries1
+print(Southasia)
